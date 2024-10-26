085db46bfc987bd7682d85b9ff09c4386b2290d5
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2024-10-26 10:11:53 +0600

    Add files via upload

---
+++ Age18.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.age18;
+
+import java.util.Scanner;
+
+public class Age18 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the ege-");
+        
+       int age = sc.nextInt();
+       
+    }
+}
