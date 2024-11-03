743a3c18d02df7f4bcf79f004195900c92a4468b
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2024-11-03 18:53:04 +0600

    Add files via upload

--- DecreasingTrianglePattern.java
+++ DecreasingTrianglePattern.java
@@ -13,7 +13,7 @@ public class DecreasingTrianglePattern {
          
          for(int i=rows;i>=1;i--){
              for(int j=1;j<=i;j++){
-                 System.out.print("# ");
+                 System.out.print(" # ");
              }
              System.out.println();
          }
