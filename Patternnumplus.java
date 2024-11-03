3fc4c5649d32d7a2e86f0a23377c9e4199dcc50a
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2024-11-03 18:54:52 +0600

    Add files via upload

---
+++ Patternnumplus.java
@@ -0,0 +1,18 @@
+/*
+ 1******
+12*****
+123****
+1234***
+12345**
+123456*
+1234567
+ */
+
+package com.mycompany.patternnumplus;
+
+public class Patternnumplus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
