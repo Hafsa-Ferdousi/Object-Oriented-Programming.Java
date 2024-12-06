1ee38f27ac491fb812565c9765730b5dbb257a31
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2024-12-07 00:55:55 +0600

    Add files via upload

---
+++ class/Cat.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package catnameagegetset;
+
+/**
+ *
+ * @author Asus
+ */
+public class Cat {
+    private String name;
+    private int age;
+    public Cat(){
+      this.name="Tom";
+      this.age=1;
+      
+    }
+    public String getA(){
+        return name;
+    }
+    public int getB(){
+        return age;
+    }
+}
---
+++ class/CatNameageGetSet.java
@@ -0,0 +1,14 @@
+
+package catnameagegetset;
+
+
+public class CatNameageGetSet {
+
+    
+    public static void main(String[] args) {
+        Cat c=new Cat();
+        System.out.println(c.getA());
+        System.out.println(c.getB());
+    }
+    
+}
