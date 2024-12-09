458fcb130f13ffc1a9a4ae8ad74162c46bd916d2
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2024-12-09 20:28:22 +0600

    Add files via upload

---
+++ random practice/Number.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.number;
+
+import java.util.Scanner;
+public class Number {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("number-");
+        int n=sc.nextInt();
+        for(int i=0;i<=n;i++){
+            System.out.println("0");
+        }
+            
+    }
+}
---
+++ random practice/Order.java
@@ -0,0 +1,18 @@
+
+
+package com.mycompany.order;
+
+import java.util.Scanner;
+public class Order {
+
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        
+        System.out.print("number-");
+        int n =Sc.nextInt();
+        for (int i=0;i<n;i++){
+            System.out.println(i);
+        }
+            
+    }
+}
