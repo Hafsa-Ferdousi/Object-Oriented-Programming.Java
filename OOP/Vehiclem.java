f3c54c865e515fd96bc949f8f61f5b3fcb8d4efc
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-01-11 22:52:06 +0600

    Add files via upload

---
+++ OOP/Vehiclem.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package vehicle;
+
+/**
+ *
+ * @author Asus
+ */
+public interface Vehiclem {
+    public void start();
+    public void stop();
+    
+}
