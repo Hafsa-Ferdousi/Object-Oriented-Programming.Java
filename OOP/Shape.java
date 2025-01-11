0154b9df63b54f1858878c75bfc3aa9d98f47eca
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-01-11 22:30:49 +0600

    Add files via upload

---
+++ OOP/Shape.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfshape;
+
+/**
+ *
+ * @author Asus
+ */
+abstract class Shape {
+   private double area;
+   
+   public double getArea(){
+       return area;
+   }
+   public void setArea(double area){
+       this.area=area;
+   }
+}
