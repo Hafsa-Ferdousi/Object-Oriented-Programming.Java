77f7876f99b522d2bb2dbf3512db51e05e750191
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-01-11 22:43:50 +0600

    Add files via upload

---
+++ OOP/16/Mouse.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mousetest;
+
+/**
+ *
+ * @author Asus
+ */
+public class Mouse {
+    public static void leftClick(){
+        System.out.println("click");
+    }
+    
+    public static void rightClick(){
+        System.out.println("clickr");
+        
+    }
+}
