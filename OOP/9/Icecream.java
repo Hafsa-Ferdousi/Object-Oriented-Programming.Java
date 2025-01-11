24ebf33f2c96e3d8c92f5d05e6dced49d0d619d7
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-01-11 22:37:12 +0600

    Add files via upload

---
+++ OOP/9/Icecream.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package icetest;
+
+/**
+ *
+ * @author Asus
+ */
+public class Icecream {
+  private String icecreamType;  
+  private String icecreamCompany;
+  private double icecreamPrice;
+  
+}
