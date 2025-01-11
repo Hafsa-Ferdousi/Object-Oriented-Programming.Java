c1c5e6e2ee51802f1627b904bdd32e4acd9d54d8
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-01-11 22:43:33 +0600

    Add files via upload

---
+++ OOP/16/Mouse1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mousetest;
+
+/**
+ *
+ * @author Asus
+ */
+public class Mouse1 extends Mouse {
+    String color ="black";
+    
+    public void setColor(){
+        System.out.println(color);
+    }
+    
+}
---
+++ OOP/16/MouseTest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mousetest;
+
+/**
+ *
+ * @author Asus
+ */
+public class MouseTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Mouse1 m1=new Mouse1();
+        
+        m1.leftClick();
+        m1.rightClick();
+    }
+    
+}
