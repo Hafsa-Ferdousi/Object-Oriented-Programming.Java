4011536719aef9c72b35cda46f259f4a5ee3da7c
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-21 16:00:19 +0600

    Update Magazine.java

--- OOP/15/Magazine.java
+++ OOP/15/Magazine.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package librarymanagementsytem;
 
-/**
- *
- * @author Asus
- */
 public class Magazine extends Item{
     private int issueNumber;
     public Magazine(String title, String author,int issueNumber) {
