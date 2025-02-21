ddf3403d199147ce5e9ade9ea54837437dfea9c2
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-21 15:33:09 +0600

    Update Animal.java

--- OOP/1/Animal.java
+++ OOP/1/Animal.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package testeranimal;
 
-/**
- *
- * @author Asus
- */
+
 abstract class Animal {
  String name;
  public Animal (String name){
