60bd9a53bcb60f4f696b4ebbd1d96aa8864e53dd
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-21 15:45:44 +0600

    Update SugerPurchase.java

--- OOP/11/SugerPurchase.java
+++ OOP/11/SugerPurchase.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package inheritance3;
 
-/**
- *
- * @author Asus
- */
 public class SugerPurchase extends PurchaseSystem  {
     double sugerWeight;
     
