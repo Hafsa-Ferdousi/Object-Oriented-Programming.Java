f2afc23a6826a8299271fb2fb90cc092f0f9cf7f
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-21 15:47:46 +0600

    Update RegulerPay.java

--- OOP/12/RegulerPay.java
+++ OOP/12/RegulerPay.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package interface2;
 
-/**
- *
- * @author Asus
- */
+
 public class RegulerPay extends PaySystem{
     
     public RegulerPay(double hour, double rate) {
