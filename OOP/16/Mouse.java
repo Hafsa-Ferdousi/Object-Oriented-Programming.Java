e5da65b8b8f1a212b72eb80e769a186bfef56682
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-21 16:02:00 +0600

    Update Mouse.java

--- OOP/16/Mouse.java
+++ OOP/16/Mouse.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package mousetest;
 
-/**
- *
- * @author Asus
- */
+
 public class Mouse {
     public static void leftClick(){
         System.out.println("click");
