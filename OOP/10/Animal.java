644e63ddb37b409eb8a7a8ec9e9f25db98ca1941
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-02-21 15:40:03 +0600

    Update Animal.java

--- OOP/10/Animal.java
+++ OOP/10/Animal.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package inheritance1;
 
-/**
- *
- * @author Asus
- */
+
 public class Animal extends  Organism  {
    public String eat;
 
