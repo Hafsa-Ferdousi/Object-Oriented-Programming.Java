afeb3da5d63051238c165aa7dba3bf74f80741d4
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-03-01 13:45:28 +0600

    Add files via upload

---
+++ Method Chaining/3/Example.java
@@ -0,0 +1,13 @@
+
+package example;
+
+
+public class Example {
+
+    public static void main(String[] args) {
+       A a=new A();
+       
+       a.setint(21).setfloat(10).display();
+    }
+    
+}
