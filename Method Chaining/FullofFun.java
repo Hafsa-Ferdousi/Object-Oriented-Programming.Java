e431903c530618cd1381631bdd73f82c45a9c1a0
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-03-01 13:42:42 +0600

    Add files via upload

---
+++ Method Chaining/FullofFun.java
@@ -0,0 +1,20 @@
+
+package fun;
+
+
+public class FullofFun {
+    
+    public FullofFun FunA(){
+        System.out.println("idk");
+        return this;
+    }
+   public FullofFun FunB(){
+       System.out.println("Sorry");
+       return this;
+   }
+   
+   public FullofFun FunC(){
+       System.out.println("emni");
+       return this;
+   }
+}
---
+++ Method Chaining/Fun.java
@@ -0,0 +1,13 @@
+
+package fun;
+
+
+public class Fun {
+
+   
+    public static void main(String[] args) {
+       FullofFun ha=new FullofFun();
+       ha.FunA().FunB().FunC();
+    }
+    
+}
